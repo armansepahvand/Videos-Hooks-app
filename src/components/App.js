19b86d62b167d601f058595bf7177d161a6ecac1
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-14 13:33:57 -0700

    Comments added

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,34 @@ import useVideos from "../hooks/useVideos";
 
 
 const App = () => {
+
+  //state for selected videos
   const [selectedVideo, setSelectedVideo] = useState(null);
+
+  //getting videos array and search function from useVideos hook  
   const [videos, search] = useVideos("bulls");
 
+  //set the selected video on the component mount and rerun the hook evrytime vidoes array is updated  
   useEffect(() => {
+    console.log("rendered");
     setSelectedVideo(videos[0]);
   }, [videos]);
 
   return (
+
     <div className="ui container" style={{ marginTop: "10px" }}>
+      
+      {/*SearchBar component with a prop that passes the search function to the component*/ }
       <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
+            {/*VideoDetail component which passes the selected vido as a prop*/}
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
+
+            {/*VideoList componenet which passes the selected video and list of videos to the component*/}
             <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
